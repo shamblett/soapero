ccbf2546f57ae2b71cdc0142f680fdd99eff5fb8
Steve Hamblett <steve.hamblett@linux.com>
2025-05-13 11:12:29 +0100

    Update build script

--- examples/OpenLDBWS/build.sh
+++ examples/OpenLDBWS/build.sh
@@ -4,17 +4,12 @@ SOAPERO_PATH=../../cmake-build-debug/soapero
 SOAPERO_RES_PATH=../../resources/
 
 SERVICE_NAME=OpenLDBWS
-#WSDL_URL=https://www.w3schools.com/xml/tempconvert.asmx?WSDL
 WSDL_DIR=./wsdl
 SERVICE_DIR=./openldbws
 BUILD_DIR=./build
 
-# Download WSDL
-#rm -rf ${WSDL_DIR}
-#mkdir -p ${WSDL_DIR}
-#wget "${WSDL_URL}" -O ${WSDL_DIR}/${SERVICE_NAME}.wsdl
-
 # Parse WSDL
+rm -rf ${SERVICE_DIR}
 mkdir -p ${SERVICE_DIR}
 ${SOAPERO_PATH} ${WSDL_DIR} ${SERVICE_DIR} --namespace=OpenLDBWS --output-mode=CMakeLists --resources-dir=${SOAPERO_RES_PATH} --service-name=${SERVICE_NAME}
 
